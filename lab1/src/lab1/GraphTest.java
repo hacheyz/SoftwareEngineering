3034140a3c020d00307f9ce2c4cc6bc189ea914e
frank <fyxhit2021@163.com>
2024-06-11 20:09:13 +0800

    Use Junit to test the queryBridgeWords.

---
+++ lab1/src/lab1/GraphTest.java
@@ -0,0 +1,30 @@
+package lab1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GraphTest {
+
+  @Test
+  public void queryBridgeWords() {
+    String[] words = MyUtils.readFile("./src/hello.txt");
+    Graph g = new Graph(words);
+
+    // Test Case 1
+    String result1 = g.queryBridgeWords("hello", "you");
+    assertNull(result1);
+
+    // Test Case 2
+    String result2 = g.queryBridgeWords("and", "gentle");
+    assertNull(result2);
+
+    // Test Case 3
+    String result3 = g.queryBridgeWords("and", "of");
+    assertEquals("full think", result3);
+
+    // Test Case 4
+    String result4 = g.queryBridgeWords("loved", "beauty");
+    assertEquals("your", result4);
+  }
+}
